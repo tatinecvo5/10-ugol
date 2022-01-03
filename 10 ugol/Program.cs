4c1fc89a7bc4cc70f0ef5fbd73ec38b2ea4f76f3
User <User@SERGEY>
2022-01-03 15:39:54 +0300

    Добавьте файлы проекта.

---
+++ 10 ugol/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_ugol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите градусы g=");
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите минуты m=");
+            int min = Convert.ToInt32(Console.ReadLine()); 
+            Console.Write("Введите секунды s=");
+            int sec = Convert.ToInt32(Console.ReadLine());
+            Ugol ugol = new Ugol(gradus, min, sec);
+            double totalRadian = ugol.ToRadian();
+            Console.Write("Заданный угол составит ");
+            Console.Write(totalRadian);
+            Console.WriteLine("радиан");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 10 ugol/ugol.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_ugol
+{
+    class Ugol
+    {
+        int gradus;
+        int min;
+        int sec;
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set 
+            {
+                gradus = value % 360;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                Gradus += value / 60;
+                min = value % 60;
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                Min += value / 60;
+                sec = value % 60;
+            }
+        }
+        public Ugol (int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+        public double ToRadian()
+        {
+            return (gradus + (min + sec / 60) / 60) * 0.0174;
+        }
+    }
+}
